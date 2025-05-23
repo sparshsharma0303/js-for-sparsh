2e9b0a56e1ebdc4ea409caecd41ba8f97ca7eb6a
sparsh sharma <sparshsharma0303@gmail.com>
2025-05-23 12:08:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//use to denotes the newer version of js
+// alert("hello")
+
+let name = "sparsh"
+//string 
+let age = 20
+// number, for larger number we use bigInt datatype
+let isLoggedIn = true 
+// boolean datatype 
+let temp = null
+//null is a standalone value and different from undefined 
+
+//symbol datatype is used for unique 
+
+console.log(typeof(temp))//object datatype
+
+
