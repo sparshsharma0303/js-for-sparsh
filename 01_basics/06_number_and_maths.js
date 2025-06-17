697b48cc0ac2f3502ff929c02ed7ed0febc4bd10
sparsh sharma <sparshsharma0303@gmail.com>
2025-06-17 15:08:45 +0000

    number and maths

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(5)
+// console.log(balance)
+
+// console.log(balance.toString())
+
+
+
+// -----------------------------------------math--------------------------------------------
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(-4.2))
+max = 20
+min = 10
+console.log(Math.floor(Math.random()*(max-min+1)+min))
