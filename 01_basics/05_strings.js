3e5534f0bebfd6281f865603631bde458d097588
sparsh sharma <sparshsharma0303@gmail.com>
2025-06-17 14:42:24 +0000

    strings and memory

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "sparsh"
+const repocount = 50 
+console.log(`hello! my name is ${name} and my repocount is ${repocount}`);
+
+const name1 = new String("sparsh")
+console.log(name1.length);
+console.log(name1.toUpperCase());
+
---
+++ 01_basics/stack_heap_memory.js
@@ -0,0 +1,18 @@
+let myYoutubeName = "sparshsharma"
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+// console.log(anotherName);
+// console.log(myYoutubeName);
+
+let userOne = {
+    userid : "use@googledotcom" ,
+    upiId : "user@ybl"
+}
+let userTwo = userOne
+userTwo.userid = "sparshsharma@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
+
+
