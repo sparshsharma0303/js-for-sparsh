f2781142bbda76a7a412c30ddfae6cb781da1272
sparsh sharma <sparshsharma0303@gmail.com>
2025-06-17 03:12:47 +0000

    datatype conversion

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,16 @@
+// score = "33"
+
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+
+let isloggedIn = true
+console.log(isloggedIn)
+Boolean = String(isloggedIn)
+console.log(typeof(Boolean))
+console.log(Boolean)
+
+let issomeNumber = 88
+let someString = String(issomeNumber) 
+console.log(someString)
